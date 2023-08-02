37f6d0d7174c4f7f2e31ccfe829b4b4c7580a1d9
Daniel Vazquez <zurdosz2012@gmail.com>
2023-08-02 12:48:45 -0300

    navbar responsive

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -2,19 +2,26 @@ import { routes } from "../Routes"
 import { Link } from "react-router-dom"
 import logoM from '../assets/logoM.jpeg'
 import './Nav.css'
-
+import React,{useState} from "react"
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <header className="container-navbar">
       <div className='logo-container'>
       <img src={logoM} alt="logo-portada" />
       <h1>Misiotrónica</h1>
       </div>
-      <div className="container-nav ">
+      <div className={`container-nav-item ${isOpen && "open"}`}>
       <Link  to={routes.home}>Inicio</Link>
       <Link  to={routes.tienda}>Tienda</Link>
       <Link  to={routes.nosotros}>Sobre Nosotros</Link>
       <Link  to={routes.contacto}>Contacto </Link>
+      </div>
+      <div className={`nav-toogle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
       </div>
     </header>
   )
